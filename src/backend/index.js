15e3dd772997e3c445fb9fb922443384c4e0e592
Stanley <stanleyli996@yahoo.com.au>
2021-10-11 21:43:45 +1100

    Stanley: Added 'node-device-detector' and can record data in log

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,10 +1,26 @@
 const winston = require('winston');
+const DeviceDetector = require('node-device-detector');
+
+const detector = new DeviceDetector;
+/** Native NodeJS */
+var http = require('http');
+var server = http.createServer(function(req) {
+    console.log(detector.detect(req.headers['user-agent']));
+    console.log('Logging results...');
+    logger.info(detector.detect(req.headers['user-agent']));
+    console.log('Logging complete');
+    });
+
+server.listen(3000, 'localhost');
+
 
 const logger = winston.createLogger({
+    format: winston.format.json(),
     transports: [
       new winston.transports.Console(),
-      new winston.transports.File({ filename: 'logs/combined.log' })
+      new winston.transports.File({ filename: 'logs/combined.log'})
     ]
 });
 
-logger.info("Hello, Winston logger, some info!");
+//console.log('result parse', detector.detect(userAgent));
+//logger.info(deviceInfo());
