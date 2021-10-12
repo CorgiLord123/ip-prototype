92f0e73b203d725e51f893299d5ed1c223c6e4a3
haoqin-xu <68982806+haoqin-xu@users.noreply.github.com>
2021-10-12 16:47:29 +1100

    add backend /data post path

--- src/backend/index.js
+++ src/backend/index.js
@@ -6,14 +6,27 @@ const port = 5000;
 
 
 const logger = winston.createLogger({
+    format: winston.format.json({ replacer: null, space: 2 }), // format newlines in log
     transports: [
       new winston.transports.Console(),
       new winston.transports.File({ filename: 'logs/combined.log' })
     ]
 });
 
-logger.info("Hello, Winston logger, some info!");
+logger.info("Logger activated");
+
+// HTTP POST REQUEST; POST METADATA FROM FRONTEND TO BACKEND; PROCESS AND LOG TO FILE
+app.post('/data', function (req, res) {
+  try {
+    const data = req.body;
+ 
+    logger.info(data);
+    res.sendStatus(200);
+  } catch {
+    res.sendStatus(500);
+  }
+});
 
 app.listen(port, () => {
-    console.log(`Backend listening at http://localhost:${port}`)
+  console.log(`Backend listening at http://localhost:${port}`)
 })
