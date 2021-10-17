48a8ed10da86a6e363aebf4ecbac1326d0331747
Stanley <stanleyli996@yahoo.com.au>
2021-10-17 19:22:15 +1100

    Stanley: stuff

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const winston = require('winston');
+const DeviceDetector = require('node-device-detector');
 
-const port = 5000;
+const port = 3000;
 
+const detector = new DeviceDetector;
 
 const logger = winston.createLogger({
     format: winston.format.json({ replacer: null, space: 2 }), // format newlines in log
@@ -15,10 +17,19 @@ const logger = winston.createLogger({
 
 logger.info("Logger activated");
 
+/*
+var http = require('http');
+var server = http.createServer(function(req) {
+    logger.info(detector.detect(req.headers['user-agent']));
+    });
+    
+server.listen(3000, 'localhost');
+*/
 // HTTP POST REQUEST; POST METADATA FROM FRONTEND TO BACKEND; PROCESS AND LOG TO FILE
 app.post('/data', function (req, res) {
   try {
-    const data = req.body;
+
+    const data = detector.detect(req.headers['user-agent']);
  
     logger.info(data);
     res.sendStatus(200);
@@ -26,6 +37,10 @@ app.post('/data', function (req, res) {
     res.sendStatus(500);
   }
 });
+
+app.get('/', function (req, res) {
+    res.send('GET request to the homepage')
+  })
 
 app.listen(port, () => {
   console.log(`Backend listening at http://localhost:${port}`)
