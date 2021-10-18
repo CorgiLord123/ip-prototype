8d72bce2740d66ed64b92a0294c439ea2b2b75ff
Stanley <stanleyli996@yahoo.com.au>
2021-10-18 21:02:28 +1100

    Stanley: Get basic bootstrap prepared and simplify imports

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,5 +1,5 @@
- import React, { useState } from 'react';
- import Card from 'react-bootstrap/Card';
+import React, { useState } from 'react';
+import { Col, Row, Card, Form, Button } from 'react-bootstrap';
  
  function LoginForm( {Login, error} ) {
 
@@ -11,30 +11,34 @@
     }
 
      return (
-        <form onSubmit={submitHandler}>
-            <div className="form-inner">
+        <Form onSubmit={submitHandler}>
                 <Card>
                     <Card.Header>Login</Card.Header>
                     <Card.Body>
                         {/* ERROR! */}
-                        <div className="form-group">
-                            <label htmlFor="name">Name:</label>
-                            <input type="text" name="name" id="name"onChange={e => setDetails({...details, name: e.target.value})} value={details.name}/>
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="email">Email:</label>
-                            <input type="email" name="email" id="email"onChange={e => setDetails({...details, email: e.target.value})} value={details.email}/>
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="password">Password:</label>
-                            <input type="password" name="password" id="password"onChange={e => setDetails({...details, password: e.target.value})} value={details.password}/>
-                        </div>
-                        <input type="submit" value="LOGIN" />
+                        <Form.Group as={Row} className="mb-3"> 
+                            <Form.Label column sm={2}>Name: </Form.Label>
+                            <Col sm={10}>
+                                <Form.Control type="text" name="name" id="name" onChange={e => setDetails({...details, name: e.target.value})} value={details.name}/>
+                            </Col>
+                        </Form.Group>
+                        <Form.Group as={Row} className="mb-3"> 
+                            <Form.Label column sm={2}>Email: </Form.Label>
+                            <Col sm={10}>
+                                <Form.Control type="email" name="email" id="email"onChange={e => setDetails({...details, email: e.target.value})} value={details.email}/>
+                            </Col>
+                        </Form.Group>
+                        <Form.Group as={Row} className="mb-3"> 
+                            <Form.Label column sm={2}>Password: </Form.Label>
+                            <Col sm={10}>
+                                <Form.Control type="password" name="password" id="password"onChange={e => setDetails({...details, password: e.target.value})} value={details.password}/>
+                            </Col>
+                        </Form.Group>
+
+                        <Button type="submit" value="LOGIN">Login</Button>
                     </Card.Body>
                 </Card>
-            </div>
-
-        </form>
+        </Form>
      )
  }
  
