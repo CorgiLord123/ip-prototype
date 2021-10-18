319238e3c66f24ee7e840dd6fa89396b24570928
Stanley <stanleyli996@yahoo.com.au>
2021-10-18 17:00:21 +1100

    Stanley: Merged with main, commented out my additions

--- src/backend/index.js
+++ src/backend/index.js
@@ -3,40 +3,29 @@ const app = express();
 const winston = require('winston');
 const DeviceDetector = require('node-device-detector');
 
-<<<<<<< HEAD
-const detector = new DeviceDetector;
-/** Native NodeJS */
-var http = require('http');
-var server = http.createServer(function(req) {
-    console.log(detector.detect(req.headers['user-agent']));
-    console.log('Logging results...');
-    logger.info(detector.detect(req.headers['user-agent']));
-    console.log('Logging complete');
-    });
-
-server.listen(3000, 'localhost');
-
-
-const logger = winston.createLogger({
-    format: winston.format.json(),
-=======
 const port = 5000;
 
+
+/** Native NodeJS */
+/**
 const detector = new DeviceDetector;
+var http = require('http');
+var server = http.createServer(function(req) {
+    logger.info(detector.detect(req.headers['user-agent']));
+    });
+
+server.listen(3000, 'localhost'); 
+ */
+
 
 const logger = winston.createLogger({
     format: winston.format.json({ replacer: null, space: 2 }), // format newlines in log
->>>>>>> Kenny
     transports: [
       new winston.transports.Console(),
       new winston.transports.File({ filename: 'logs/combined.log'})
     ]
 });
 
-<<<<<<< HEAD
-//console.log('result parse', detector.detect(userAgent));
-//logger.info(deviceInfo());
-=======
 logger.info("Logger activated");
 
 // HTTP POST REQUEST; POST METADATA FROM FRONTEND TO BACKEND; PROCESS AND LOG TO FILE
@@ -59,4 +48,3 @@ app.get('/', function (req, res) {
 app.listen(port, () => {
   console.log(`Backend listening at http://localhost:${port}`)
 })
->>>>>>> Kenny
